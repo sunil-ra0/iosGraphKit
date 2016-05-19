3178f8b42d63d0907188fb8570a3545cc1b779e5
SunilRao <sunil.s@razorthink.net>
2016-05-19 18:25:41 +0530

    Removed commented code

--- GraphKit/View Controllers/FirstViewController.m
+++ GraphKit/View Controllers/FirstViewController.m
@@ -53,20 +53,4 @@
     self.lineGraphView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height);
     
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
